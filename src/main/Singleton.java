1c34415a8f0c06b7e10e52b2474c5d744f6586e0
notishell <notishell@users.noreply.github.com>
2024-03-30 15:12:41 +0800

    feat: singleton pattern demo and unit tests

---
+++ src/main/Singleton.java
@@ -0,0 +1,45 @@
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * 单例模式
+ * Singleton pattern
+ *
+ * @author Notis Hell notishell@gmail.com 2024/03/29 18:48:44
+ */
+public class Singleton implements Serializable {
+	/**
+	 * 私有静态实例，防止被引用，此处赋值为null，目的是实现延迟加载
+ 	 */
+	private static volatile Singleton instance = null;
+
+	/**
+	 * 私有构造方法，防止被实例化
+	 */
+	private Singleton() {
+	}
+
+	/**
+	 * 静态工程方法，创建实例
+ 	 */
+	public static Singleton getInstance() {
+		if (Objects.nonNull(instance)) {
+			return instance;
+		}
+		synchronized(Singleton.class) {
+			if (Objects.isNull(instance)) {
+				instance = new Singleton();
+			}
+		}
+		return instance;
+	}
+
+	/**
+	 * 如果该对象被用于序列化，可以保证对象在序列化前后保持一致
+ 	 */
+	@Serial
+	public Object readResolve() {
+		return getInstance();
+	}
+}
---
+++ src/test/SingletonTests.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.io.*;
+
+/**
+ * 单例模式测试
+ * Test for singleton pattern
+ *
+ * @author Notis Hell notishell@gmail.com 2024/03/29 18:54:02
+ */
+public class SingletonTests {
+
+	@Test
+	public void should_getInstance_return_notNull() {
+		Singleton singleton = Singleton.getInstance();
+
+		Assert.notNull(singleton, "return null");
+	}
+
+	@Test
+	public void should_getInstance_return_same() {
+		Singleton singleton1 = Singleton.getInstance();
+		Singleton singleton2 = Singleton.getInstance();
+
+		Assert.isTrue(singleton1 == singleton2, "not same instance");
+	}
+
+	@Test
+	public void should_getInstance_after_deserial_return_same() throws IOException, ClassNotFoundException {
+		Singleton singleton1 = Singleton.getInstance();
+
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+		objectOutputStream.writeObject(singleton1);
+
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+		ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+
+		Singleton singleton2 = (Singleton)objectInputStream.readObject();
+
+		Assert.isTrue(singleton1 == singleton2, "not same instance");
+	}
+
+}
